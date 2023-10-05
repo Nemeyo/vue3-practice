974308b580bdd3af5c1b6098f2ffc8f77bc9cd29
Nemeyo <ssss4531@gmail.com>
2023-10-05 14:08:47 +0800

    elementPlus自動引入

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//elements按需導入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     // 實際路徑轉換
