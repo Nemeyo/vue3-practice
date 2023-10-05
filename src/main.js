819cc8dd27bc3e66e2c9594f7adc1397744ed93f
Nemeyo <ssss4531@gmail.com>
2023-10-05 14:51:04 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/main.css'
+// 測試接口函數
+import { getCategary } from '@/apis/testAPI'
+getCategary().then(res => {
+    console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
